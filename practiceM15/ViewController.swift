23898f6aa0086bc9c5fccdbd1eac30e9fbaf8def
Moroz-max <max311097@mail.ru>
2023-12-17 18:59:18 +0300

    Initial Commit

---
+++ practiceM15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practiceM15
+//
+//  Created by Максим Морозов on 17.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
