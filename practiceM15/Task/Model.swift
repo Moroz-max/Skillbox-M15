233692a8eca0380a1d66cb62c43c1376f2cb5fab
Maxim Morozov <moroz@nonstudio.ru>
2023-12-18 06:35:43 +0300

    done

---
+++ practiceM15/Task/Model.swift
@@ -0,0 +1,16 @@
+//
+//  Model.swift
+//  practiceM15
+//
+//  Created by Максим Морозов on 18.12.2023.
+//
+
+import Foundation
+import UIKit
+
+struct PostModel {
+    let postImage: UIImage
+    let postTitle: String
+    let postText: String
+    let postTimeAfter: String
+}
--- practiceM15/Task/ViewController.swift
+++ practiceM15/Task/ViewController.swift
@@ -12,10 +12,38 @@ import Alamofire
 class ViewController: UIViewController {
     
     let cellID = "cell"
+    
+    private lazy var tableView = {
+        let tableView = UITableView(frame: CGRect.zero, style: .insetGrouped)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.separatorStyle = .singleLine
+        tableView.allowsSelection = false
+        return tableView
+    }()
+    
+    let models: [PostModel] = [PostModel(postImage: UIImage(), postTitle: "Header", postText: "He'll want to use your yacht, and I don't want this thing smelling like fish.", postTimeAfter: "8m ago"),
+                               PostModel(postImage: UIImage(), postTitle: "Header", postText: "He'll want to use your yacht, and I don't want this thing smelling like fish.", postTimeAfter: "8m ago")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.register(CustomCell.self, forCellReuseIdentifier: cellID)
+        tableView.delegate = self
+        tableView.dataSource = self
+        setupViews()
+        setupConstraints()
+    }
+    
+    private func setupViews() {
+        view.addSubview(tableView)
+    }
+    
+    private func setupConstraints() {
+        tableView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.left.equalToSuperview()
+            make.right.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
     }
 
 
@@ -24,14 +52,20 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return models.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? CustomCell ?? UITableViewCell()
-        
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath) as? CustomCell
+        cell?.configure(models[indexPath.row])
+        return cell ?? UITableViewCell()
     }
     
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+extension ViewController: UITableViewDelegate {
     
 }
